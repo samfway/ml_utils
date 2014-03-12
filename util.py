53fe5d7f293ee26c85bb678a79459f558a96bb33
samfway <samfway@gmail.com>
2014-03-12 11:02:46 -0600

    Added label to index conversion

--- util.py
+++ util.py
@@ -12,6 +12,12 @@ __status__ = "Development"
 """ Machine Learning utility script 
 """
 
+def convert_labels_to_int(labels):
+    """ Convert a list of labels to indices """ 
+    unique_labels = list(set(labels))
+    label_indices = [ unique_labels.index(l) for l in labels ] 
+    return unique_labels, label_indices
+
 def bool_cast(s):
     """ Cast string to boolean """
     if s.lower() == 'true' or s.lower() == 't':
