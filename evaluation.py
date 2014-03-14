8c616b4191cb5bb1978c8d341d833119abc592ee
samfway <samfway@gmail.com>
2014-03-14 15:12:19 -0600

    More eval stuff

--- evaluation.py
+++ evaluation.py
@@ -12,10 +12,10 @@ __status__ = "Development"
 """ Scripts for evaluating scikit-learn models 
 """
 
-from .cross_validation import get_test_train_set
+from cross_validation import get_test_train_set
 from time import clock 
 
-def get_evaluation_report(models, data_matrix, labels, test_sets, metrics)
+def get_evaluation_report(models, data_matrix, labels, test_sets, metrics):
     """ Get prediction results for a given list of models, using the supplied test sets 
         Inputs:
             models       : list of tuples, each tuple containing a name and model 
@@ -27,9 +27,12 @@ def get_evaluation_report(models, data_matrix, labels, test_sets, metrics)
     """
 
     evaluation_report = {} 
+    train_time_key = 'Training time (s)'
+    run_time_key = 'Running time (s)'
+
     for model_name, model in models:
         # For each model, measure train/run time and all supplied metrics
-        evaluation_report[model_name] = {'train_time':[],'run_time':[]}
+        evaluation_report[model_name] = {train_time_key:[],run_time_key:[]}
         for metric_name, metric in metrics:
             evaluation_report[model_name][metric_name] = [] 
         
@@ -44,17 +47,38 @@ def get_evaluation_report(models, data_matrix, labels, test_sets, metrics)
             start_time = clock() 
             model.fit(training_matrix, training_labels)
             stop_time = clock() 
-            evaluation_report[model_name]['train_time'].append(stop_time-start_time)
+            evaluation_report[model_name][train_time_key].append(stop_time-start_time)
 
             # Testing 
             start_time = clock()
             predictions = model.predict(test_matrix)
             stop_time = clock() 
-            evaluation_report[model_name]['run_time'].append(stop_time-start_time)
+            evaluation_report[model_name][run_time_key].append(stop_time-start_time)
 
             # Evaluation 
             for metric_name, metric in metrics: 
                 evaluation_report[model_name][metric_name].append(metric(test_labels, predictions))
             
     return evaluation_report 
+
+def print_evaluation_report(evaluation_report, output_file):
+    """ Write an evaluation report out to a text file """ 
+    model_names = evaluation_report.keys()
+    metrics = evaluation_report[model_names[-1]].keys()
+
+    output = open(output_file, 'w')
+    output.write('Models:\t' + '\t'.join(model_names))
+
+    for metric in metrics:
+        output.write('\n%s:' % (metric))
+        for model_name in model_names:
+            temp = array(evaluation_report[model_name][metric])
+            output.write('\t%.2f (+-%.2f)' % (temp.mean(), temp.std()))
+
+    output.close()     
+
+def make_evaluation_report(models, data_matrix, labels, test_sets, metrics, output_file):
+    """ Run and print evaluation report """ 
+    evaluation_report = get_evaluation_report(models, data_matrix, labels, test_sets, metrics)
+    print_evaluation_report(evaluation_report, output_file)
 
