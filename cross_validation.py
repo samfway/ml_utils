23e9156756ba93c36eea16a5a874ee3ced9f4be5
samfway <samfway@gmail.com>
2014-03-14 13:18:41 -0600

    Added saving/loading of test sets

--- cross_validation.py
+++ cross_validation.py
@@ -13,6 +13,7 @@ __status__ = "Development"
 """
 
 from sklearn.cross_validation import StratifiedKFold, KFold
+from numpy import array
 
 def get_test_sets(class_labels, kfold=10, stratified=True):
     """ Generate lists of indices for performing k-fold cross validation
@@ -23,6 +24,32 @@ def get_test_sets(class_labels, kfold=10, stratified=True):
         return StratifiedKFold(class_labels, kfold)
     else: 
         return KFold(len(class_labels), kfold)
+
+def save_test_sets_to_file(test_sets, output_file):
+    """ Save train/test indices to a file 
+        Each pair of lines denotes the training and testing 
+        indices for a single train-test set 
+    """ 
+    output = open(output_file, 'w')
+    for train_idx, test_idx in test_sets:
+        output.write(','.join([ str(i) for i in train_idx ]) + '\n')
+        output.write(','.join([ str(i) for i in test_idx ]) + '\n')
+    output.close()
+
+def load_test_sets_from_file(input_file):
+    """ Read in test sets from file """ 
+    test_sets = [] 
+    temp_set = {'train':None, 'test':None}
+    currently_parsing = 'train' # start by parsing training
+
+    for line in open(input_file, 'rU'):
+        temp_set[currently_parsing] = array([int(x) for x in line.split(',')])
+        if currently_parsing == 'test': 
+            test_sets.append( (temp_set['train'], temp_set['test']) )
+            currently_parsing = 'train'
+        else:
+            currently_parsing = 'test'
+    return test_sets
 
 def get_test_train_set(data_matrix, actual_values, test_set, is_distance_matrix=False):
     """ Create test/train data set for cross-validation 
